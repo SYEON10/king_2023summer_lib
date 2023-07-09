03f08422fac1cce768462b7a2e2a03825198561c
Ksyeon <syeon.dev@gmail.com>
2023-07-09 17:36:15 +0900

    [Delete Cache]
    .gitignore 적용이 안됨을 확인. 캐시 삭제 전 백업용 커밋.

--- Library project/Assets/Scripts/KSY/FileIO/JsonFile.cs
+++ Library project/Assets/Scripts/KSY/FileIO/JsonFile.cs
@@ -1,13 +1,17 @@
 /*
+ [Not Finished]
 Programmer : KangSYEON
       Date : 7/7/2023
    Purpose : Read Json File
 */
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using Newtonsoft.Json;
+
 
 #region About Json File
 /*
@@ -15,7 +19,7 @@ Json 파일로 객체를 저장/불러오기를 할 수 있습니다.
 Json 파일은 가시적으로 보기 편하고, XML보다 가볍다는 장점이 있습니다.
 
 본 JsonFile은 Json 파일을 읽어오기 위해 작성되었다.
-클래스의 형태를 가진 정적 데이터를 읽어오기 위한 목적으로 만들어졌기 때문에 Write는 불가능하다.
+클래스의 형태를 가진 정적 데이터를 읽어오기 위한 목적으로 만들어졌기 때문에 Write는 불가능하다. (사실 플머의 역량부족... )
 동적 데이터의 저장은 .ini 파일을 이용하면 된다.
  */
 #endregion
@@ -36,9 +40,14 @@ public class JsonFile
 
     }
 
+    public void Write()
+    {
+        
+    }
+
     public T Read<T>(){
         string content = File.ReadAllText(_filePath);
-        return JsonUtility.FromJson<T>(content);
+        return JsonConvert.DeserializeObject<T>(content);
     }
     
 }
