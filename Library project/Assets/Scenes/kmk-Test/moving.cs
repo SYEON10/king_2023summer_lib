3b1728cfdac34da6d3bd74fae6a9735f8d980c44
kmk0105-S2 <11morgan@naver.com>
2023-08-08 19:54:37 +0900

    50%완성 맵

---
+++ Library project/Assets/Scenes/kmk-Test/moving.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 10.0f;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        { 
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), _speed);
+            transform.position += Vector3.forward * Time.deltaTime * _speed;
+        } 
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), _speed);
+            transform.position += Vector3.back * Time.deltaTime * _speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), _speed);
+            transform.position += Vector3.left * Time.deltaTime * _speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), _speed);
+            transform.position += Vector3.right * Time.deltaTime * _speed;
+        }
+    }
+}
--- Library project/Assets/Scripts/gamm/CamControl.cs
+++ Library project/Assets/Scripts/gamm/CamControl.cs
@@ -8,57 +8,65 @@ public class CamControl : MonoBehaviour
     public float xmove = 0; //x 누적 움직인 양
     public float ymove = 0; //y 누적 움직인 
     public float distance = 5;
-    public float toggleView = 1;
+    public enum CameraView { player1, player3, upperSideCam, rigtSideCam, leftSideCam }
+    public int camera = 0;
+    public KeyCode cameraKey = KeyCode.Alpha1; // 기본 카메라 키를 1로 설정
+
+
+
 
     void Update()
     {
-
-
-        if (Input.GetKeyDown("1"))
+        if (Input.GetKeyDown(KeyCode.T))
         {
-            toggleView = toggleView + 1;
-            if (toggleView > 4)
-                toggleView = 1;
+            Debug.Log("키가 랜덤으로 변경됩니다");
+            cameraKey = (KeyCode)Random.Range((int)KeyCode.A, (int)KeyCode.Z + 1);
         }
 
 
-        if (toggleView == 1)
+        if (Input.GetMouseButton(0))
         {
-            if (Input.GetMouseButton(1)) //우클릭, 드래그해서 화면 돌리기
-            {
-                xmove += Input.GetAxis("Mouse X"); //마우스의 좌우 이동량을 xmove에 누적
-                ymove -= Input.GetAxis("Mouse Y"); //마우스의 상하 이동량을 ymove에 누적
-            }
-            transform.rotation = Quaternion.Euler(ymove * 2, xmove * 2, 0); //이동량에 따라 카메라가 바라보는 방향 조정 , 드래그 속도가 느려서 *2했음 
-
-            Vector3 reverseDistance = new Vector3(0.0f, 0.0f, distance);
-            transform.position = player.transform.position - transform.rotation * reverseDistance;
-        }
-        if (toggleView == 2)
-        {
-            Vector3 upperSide = new Vector3(0.0f, 10, 0.0f);
-
-            transform.position = player.transform.position + upperSide;
-            transform.rotation = Quaternion.Euler(90, 0, 0);
-        }
-        if (toggleView == 3)
-        {
-
-            Vector3 rightSide = new Vector3(10, 0.0f, 0.0f);
-
-            transform.position = player.transform.position + rightSide;
-            transform.rotation = Quaternion.Euler(0, 270, 0);
+            xmove += Input.GetAxis("Mouse X");
+            ymove -= Input.GetAxis("Mouse Y");
+            transform.rotation = Quaternion.Euler(ymove * 2, xmove * 2, 0);
 
         }
 
-        if (toggleView == 4)
+ 
+
+        if (Input.GetKeyDown(cameraKey))
         {
+            camera = camera + 1;
+            if (camera == 5) { camera = 0; }
+        }
 
-            Vector3 leftSide = new Vector3(-10, 0.0f, 0.0f);
 
-            transform.position = player.transform.position + leftSide;
-            transform.rotation = Quaternion.Euler(0, 90, 0);
 
+        switch ((CameraView)camera)
+        {
+            case CameraView.player1:
+                Vector3 reverseDistance1 = new Vector3(0.0f, 0.4f, 0.2f); // 카메라가 바라보는 앞방향은 Z 축, 이동량에 따른 Z 축방향의 벡터를 구함 
+                transform.position = player.transform.position + transform.rotation * reverseDistance1;
+                break;
+            case CameraView.player3:
+                Vector3 reverseDistance3 = new Vector3(0.0f, 0.0f, distance);
+                transform.position = player.transform.position - transform.rotation * reverseDistance3;
+                break;
+            case CameraView.upperSideCam:
+                Vector3 upperSide = new Vector3(0.0f, 10, 0.0f);
+                transform.position = player.transform.position + upperSide;
+                transform.rotation = Quaternion.Euler(90, 0, 0);
+                break;
+            case CameraView.rigtSideCam:
+                Vector3 rightSide = new Vector3(10, 0.0f, 0.0f);
+                transform.position = player.transform.position + rightSide;
+                transform.rotation = Quaternion.Euler(0, 270, 0);
+                break;
+            case CameraView.leftSideCam:
+                Vector3 leftSide = new Vector3(-10, 0.0f, 0.0f);
+                transform.position = player.transform.position + leftSide;
+                transform.rotation = Quaternion.Euler(0, 90, 0);
+                break;
         }
     }
 }
