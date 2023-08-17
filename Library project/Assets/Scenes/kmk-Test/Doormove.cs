eba29f59e6ede43d5bf64376ed957772b1b5f47f
kmk0105-S2 <11morgan@naver.com>
2023-08-17 15:32:35 +0900

    70%맵
    엘베 문 움직인다 와아~~

--- Library project/Assets/Scenes/kmk-Test/Doormove.cs
+++ Library project/Assets/Scenes/kmk-Test/Doormove.cs
@@ -2,21 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Doormove : MonoBehaviour
+public class DoorMove : MonoBehaviour
 {
-    public Vector3 moveDirection = Vector3.right;
-    public float moveSpeed = 50000f;
-    public Transform Door2;
+    public Transform door1Transform;
+    public Transform door2Transform;
+    public float slideDistance = 5.0f;
+    public float slideSpeed = 2.0f;
 
+    private Vector3 initial1Position;
+    private Vector3 initial2Position;
+    private Vector3 target1Position;
+    private Vector3 target2Position;
+    private bool isDoorOpen = false;
 
-    void Start()
+    private void Start()
     {
-        
+        initial1Position = door1Transform.position;
+        initial2Position = door2Transform.position; 
+        target1Position = initial1Position + Vector3.left * slideDistance;
+        target2Position = initial2Position + Vector3.right * slideDistance;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // 마우스 왼쪽 버튼 클릭을 감지합니다.
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 클릭된 위치의 스크린 좌표를 Ray로 변환합니다.
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // Ray가 어떤 콜라이더와 충돌했는지 확인합니다.
+            if (Physics.Raycast(ray, out hit))
+            {
+                // 버튼을 클릭했을 때 문 상태를 변경합니다.
+                if (hit.collider.gameObject.name == "Up Button" || hit.collider.gameObject.name == "Down Button")
+                {
+                    ToggleDoor();
+                }
+            }
+        }
+    }
+
+    private void ToggleDoor()
+    {
+        isDoorOpen = !isDoorOpen;
+    }
+
+    private void FixedUpdate()
+    {
+        // 문 상태에 따라 문을 열거나 닫습니다.
+        Vector3 target2 = isDoorOpen ? target2Position : initial2Position;
+        Vector3 target1 = isDoorOpen ? target1Position : initial1Position;
+        door1Transform.position = Vector3.MoveTowards(door1Transform.position, target1, slideSpeed * Time.fixedDeltaTime);
+        door2Transform.position = Vector3.MoveTowards(door2Transform.position, target2, slideSpeed * Time.fixedDeltaTime);
     }
 }
+
+
