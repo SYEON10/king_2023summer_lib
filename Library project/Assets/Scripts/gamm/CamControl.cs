8197d0b6bc580c04c6c7d73aff6f497838a84d65
i-gimjo <119905015+i-gimjo@users.noreply.github.com>
2023-08-18 04:38:43 +0900

    원거리공격

--- Library project/Assets/Scripts/gamm/CamControl.cs
+++ Library project/Assets/Scripts/gamm/CamControl.cs
@@ -7,66 +7,32 @@ public class CamControl : MonoBehaviour
     public GameObject player; //따라다닐 오브젝트 지정 
     public float xmove = 0; //x 누적 움직인 양
     public float ymove = 0; //y 누적 움직인 
-    public float distance = 5;
-    public enum CameraView { player1, player3, upperSideCam, rigtSideCam, leftSideCam }
-    public int camera = 0;
-    public KeyCode cameraKey = KeyCode.Alpha1; // 기본 카메라 키를 1로 설정
-
-
+   
+    public float SmoothTime = 0.2f;
+    private Vector3 velocity = Vector3.zero;
+    private int toggleView = 3; //현재 1인칭인지, 3인칭인지 판단. 처음에는 3인칭 
 
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T))
+        
+            xmove += Input.GetAxis("Mouse X"); //마우스의 좌우 이동량을 xmove에 누적
+            ymove -= Input.GetAxis("Mouse Y"); //마우스의 상하 이동량을 ymove에 누적
+        
+        transform.rotation = Quaternion.Euler(ymove * 2, xmove * 2, 0); //이동량에 따라 카메라가 바라보는 방향 조정 , 드래그 속도가 느려서 *2했음 
+
+        if (Input.GetMouseButtonDown(2)) //휠클릭, 1/3인칭 시점 전환
+            toggleView = 4 - toggleView; //1>3 클릭때마다 변함 
+
+        if (toggleView == 1) //1인칭일때 
         {
-            Debug.Log("키가 랜덤으로 변경됩니다");
-            cameraKey = (KeyCode)Random.Range((int)KeyCode.A, (int)KeyCode.Z + 1);
+            Vector3 reverseDistance = new Vector3(0.0f, 5.0f, 0.2f); // 카메라가 바라보는 앞방향은 Z 축, 이동량에 따른 Z 축방향의 벡터를 구함 
+            transform.position = player.transform.position + transform.rotation * reverseDistance; // 플레이어의 위치에서 카메라가 바라보는 방향에 벡터값을 적용한 상대 좌표를 차감
         }
-
-
-        if (Input.GetMouseButton(0))
+        else if (toggleView == 3)
         {
-            xmove += Input.GetAxis("Mouse X");
-            ymove -= Input.GetAxis("Mouse Y");
-            transform.rotation = Quaternion.Euler(ymove * 2, xmove * 2, 0);
-
-        }
-
- 
-
-        if (Input.GetKeyDown(cameraKey))
-        {
-            camera = camera + 1;
-            if (camera == 5) { camera = 0; }
-        }
-
-
-
-        switch ((CameraView)camera)
-        {
-            case CameraView.player1:
-                Vector3 reverseDistance1 = new Vector3(0.0f, 0.4f, 0.2f); // 카메라가 바라보는 앞방향은 Z 축, 이동량에 따른 Z 축방향의 벡터를 구함 
-                transform.position = player.transform.position + transform.rotation * reverseDistance1;
-                break;
-            case CameraView.player3:
-                Vector3 reverseDistance3 = new Vector3(0.0f, 0.0f, distance);
-                transform.position = player.transform.position - transform.rotation * reverseDistance3;
-                break;
-            case CameraView.upperSideCam:
-                Vector3 upperSide = new Vector3(0.0f, 10, 0.0f);
-                transform.position = player.transform.position + upperSide;
-                transform.rotation = Quaternion.Euler(90, 0, 0);
-                break;
-            case CameraView.rigtSideCam:
-                Vector3 rightSide = new Vector3(10, 0.0f, 0.0f);
-                transform.position = player.transform.position + rightSide;
-                transform.rotation = Quaternion.Euler(0, 270, 0);
-                break;
-            case CameraView.leftSideCam:
-                Vector3 leftSide = new Vector3(-10, 0.0f, 0.0f);
-                transform.position = player.transform.position + leftSide;
-                transform.rotation = Quaternion.Euler(0, 90, 0);
-                break;
+            Vector3 reverseDistance = new Vector3(0.0f, -4.0f, 20.0f);
+            transform.position = player.transform.position - transform.rotation * reverseDistance;
         }
     }
 }
