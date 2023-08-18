cb2bedb1fa360351e2dfd3d745647f772b6eb93d
i-gimjo <119905015+i-gimjo@users.noreply.github.com>
2023-08-18 16:59:26 +0900

    Auto stash before merge of "main" and "origin/main"
    메인화면
    만드는중

--- Library project/Assets/Scripts/gamm/CamControl.cs
+++ Library project/Assets/Scripts/gamm/CamControl.cs
@@ -17,6 +17,13 @@ public class CamControl : MonoBehaviour
     private int toggleView = 3; //현재 1인칭인지, 3인칭인지 판단. 처음에는 3인칭 
 
 
+
+    void Start()
+    {
+
+    }
+
+
     void Update()
     {
         if (Input.GetMouseButtonDown(2)) //휠클릭, 1/3인칭 시점 전환
@@ -24,12 +31,18 @@ public class CamControl : MonoBehaviour
 
         if (toggleView == 1) //1인칭일때 
         {
+
+
+
+
+
             xmove += Input.GetAxis("Mouse X"); //마우스의 좌우 이동량을 xmove에 누적
             ymove -= Input.GetAxis("Mouse Y"); //마우스의 상하 이동량을 ymove에 누적
 
             transform.rotation = Quaternion.Euler(ymove * 2, xmove * 2, 0); //이동량에 따라 카메라가 바라보는 방향 조정 , 드래그 속도가 느려서 *2했음 
             Vector3 reverseDistance = new Vector3(0.0f, 5.0f, 1.0f); // 카메라가 바라보는 앞방향은 Z 축, 이동량에 따른 Z 축방향의 벡터를 구함 
             transform.position = player.transform.position + transform.rotation * reverseDistance; // 플레이어의 위치에서 카메라가 바라보는 방향에 벡터값을 적용한 상대 좌표를 차감
+
         }
         else if (toggleView == 3)
         {
@@ -41,6 +54,7 @@ public class CamControl : MonoBehaviour
             transform.rotation = Quaternion.Euler(ymove * 2, xmove * 2, 0); //이동량에 따라 카메라가 바라보는 방향 조정 , 드래그 속도가 느려서 *2했음 
             Vector3 reverseDistance1 = new Vector3(0.0f, 6.0f, -20.0f);
             transform.position = player.transform.position + transform.rotation * reverseDistance1;
+ 
         }
     }
 }
